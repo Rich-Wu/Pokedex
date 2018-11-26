3f920d63b899a32cbf9dac0f07e299441af4b25c
Richard Wu <Richie1988@gmail.com>
2018-11-26 01:21:56 -0500

    fixing bugs

--- js/main.js
+++ js/main.js
@@ -101,7 +101,6 @@ function addPokemon() {
       var pokemon = new Pokemon(data['id'], data['name'], data['sprites'], data['weight'], data['height'], data['types'], data['stats'][5]['base_stat'], data['stats'][4]['base_stat'], data['stats'][3]['base_stat'], data['stats'][2]['base_stat'], data['stats'][1]['base_stat'], data['stats'][0]['base_stat'], data['abilities']);
       // console.log(pokemon);
       pokemon['flavorText'] = addFlavor(pokemon);
-      setTimeout(drawCarousel, 400);
     };
     if (this.readyState == 4 && this.status != 200) {
       alert('An error occurred while processing your input of ' + userEntry + '. Please try another input.');
@@ -148,7 +147,6 @@ function addFlavor(pokemon) {
             // console.log('still good');
           }
           Red.pokes[pokemon['number']] = pokemon;
-          // drawPokeballs();
           return;
         }
       }
